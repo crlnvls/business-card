9cbcfa3a308862a79feb9c704540a7e99d3b14f0
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-24 22:48:06 +0000

    Add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Card from "./pages/Card";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Card />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 };

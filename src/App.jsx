6b04a4d3245afce9a0772aeb805846a0177c586e
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-24 22:42:24 +0000

    Add routes and card page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import Card from "./pages/Card";
+import "./App.css";
+
 const App = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Card />}></Route>
+    </Routes>
+  );
 };
 
 export default App;

0c0d763b3591be8b1da1e2709fd9e1d1821c2250
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-24 23:41:43 +0000

    Fix footer styling

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 
+import "./Card.css";
+
 const Card = () => {
   return (
     <>
-      <div>
+      <div className="main">
         <div>
           <img src="/photo.jpeg" alt="photo" />
           <h1> Caroline Veloso</h1>
           <h2>Software Developer</h2>
           <button>Email</button>
+          <h3>About</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+            voluptatibus quo impedit sapiente libero perspiciatis dolore et,
+            nostrum aut, laudantium molestias. Exercitationem consequuntur
+            illum, laboriosam dolore laborum ea soluta eius?
+          </p>
+          <h3>Interests</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+            reiciendis est reprehenderit optio quidem nemo, quas consequuntur
+            ut? Saepe at cum nam impedit iusto qui! Quod, ipsam itaque! Nam,
+            minus?
+          </p>
         </div>
       </div>
     </>

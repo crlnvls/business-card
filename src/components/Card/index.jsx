0d714159debc787567dc2b465dca970870775297
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-25 17:23:55 +0000

    Add socials btn component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import EmailIcon from "@mui/icons-material/Email";
 import "./Card.css";
@@ -12,22 +11,28 @@ const Card = () => {
         <div className="center-div">
           <h1 className="name"> Caroline Veloso</h1>
           <h2 className="job">Software Developer</h2>
-          <Link to="cveloso@hotmail.co.uk" className="email">
-            <EmailIcon className="icon" sx={{ fontSize: 22 }} />
+          <a
+            href="https://crlnvls.github.io/"
+            className="website"
+            target="_blank"
+          >
+            crlnvls.website
+          </a>
+          <a href="mailto:cveloso@hotmail.co.uk" className="email">
+            <EmailIcon className="icon" sx={{ fontSize: 19 }} />
             Email
-          </Link>
+          </a>
           <div className="text">
             <h3 className="heading">About</h3>
             <p className="info">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
               voluptatibus quo impedit sapiente libero perspiciatis dolore et,
-              nostrum aut, laudantium molestias. Exercitationem consequuntur
+              nostrum aut, laudantium.
             </p>
             <h3 className="heading">Interests</h3>
             <p className="info">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-              reiciendis est reprehenderit optio quidem nemo, quas consequuntur
-              ut? Saepe at cum nam impedit iusto qui! Quod,
+              reiciendis est reprehenderit optio quidem nemo.
             </p>
           </div>
         </div>
---
+++ src/components/SocialBtn/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./SocialBtn.css";
+
+const SocialBtn = () => {
+  return;
+};
+
+export default SocialBtn;

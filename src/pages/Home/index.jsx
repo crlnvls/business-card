4a7b7ef8188de005bd48fa4f7d52795583552df0
“crlnvls” <cveloso@hotmail.co.uk>
2023-03-01 12:27:42 +0000

    Add dark/light mode

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 
 import Card from "../../components/Card";
+import "./Home.css";
+import { createContext, useState } from "react";
+import Switch from "@mui/material/Switch";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
+
+export const ThemeContext = createContext(null);
 
 const Home = () => {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "dark" ? "light" : "dark"));
+  };
   return (
     <>
-      <Card />
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <div className="color-theme" id={theme}>
+          <div className="switch">
+            <p className="border">
+              <BedtimeIcon />
+              <Switch color="default" onChange={toggleTheme} />
+              <WbSunnyIcon />
+            </p>
+          </div>
+          <Card />
+        </div>
+      </ThemeContext.Provider>
     </>
   );
 };
